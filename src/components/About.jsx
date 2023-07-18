dc96770f85d5441af87a600206b6ad0558347530
raheem6 <raheemabuhumra@csus.edu>
2023-07-18 12:57:35 -0700

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,39 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import {fadeIn, textVariant} from '../utils/motion';
+import { sectionWrapper } from '../hoc';
 
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
 
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <>
+    <div>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}
         >Introduction</p>
@@ -24,8 +51,14 @@ const About = () => {
       >
       I'm passionate about computer science and helping people with their queries related to it. Currently, I'm searching for an internship opportunity on Handshake and Linkedin to enhance my skills and gain practical experience in the field. I have experience in programming languages such as Java and C++, version control tools like Git and GitHub, and front-end web development with HTML, CSS, and JavaScript. Additionally, I'm multilingual and fluent in English and Arabic. Apart from my technical expertise, I'm also interested in outdoor activities like hiking, traveling, walking, and building gaming PCs.
       </motion.p>
-    </>
+      
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default About
+export default sectionWrapper(About, "about")
