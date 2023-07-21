59658e28eca2d32356eb38bce6412d9634bbbbdd
raheem6 <raheemabuhumra@csus.edu>
2023-07-20 22:03:11 -0700

    tech section updated!

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+
+import { fadeIn, textVariant } from "../utils/motion";
+import { sectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { styles } from "../styles";
+
+const TechCard = ({ index, icon }) => {
+  return (
+    <Tilt className="w-28 h-28">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-full shadow-card select-none"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-full py-5 flex justify-center items-center flex-col"
+        >
+          <img src={icon} className="w-16 h-16 object-contain" alt="technology-icon" />
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <>
+      <div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>My tools</p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Technologies.
+        </h2>
+      </div>
 
-export default Tech
+      <div className="flex flex-row flex-wrap justify-center gap-10 mt-20">
+        {technologies.map((technology, index) => (
+          <TechCard icon={technology.icon} key={technology.name} index={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default sectionWrapper(Tech, "");
