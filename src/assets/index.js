0f02fad87e413041a994c6e480c6776abfeabbc5
raheem6 <raheemabuhumra@csus.edu>
2023-07-23 14:25:53 -0700

    index updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import DuncanPIC from "./DuncanPIC.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -31,6 +32,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  DuncanPIC,
   logo,
   backend,
   creator,
