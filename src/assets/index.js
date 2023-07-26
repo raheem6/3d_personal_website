5f912d4add0b47f2de29459566551b42a11d3d5c
raheem6 <raheemabuhumra@csus.edu>
2023-07-25 17:23:40 -0700

    Assets file updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,11 +22,6 @@ import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 import DuncanPIC from "./DuncanPIC.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import janeHop from "./janeHop.png";
 import tripguide from "./tripguide.png";
@@ -54,10 +49,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   janeHop,
   tripguide,
