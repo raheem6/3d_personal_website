3eeabcc2e72fe61910cecd6e15372ab87842381b
raheem6 <raheemabuhumra@csus.edu>
2023-07-25 17:24:08 -0700

    index section updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,7 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
+import Projects from "./Projects";
 import Resume from "./Resume";
 import Contact from "./Contact";
 
@@ -13,8 +12,7 @@ export {
   Navbar,
   About,
   Tech,
-  Experience,
-  Works,
+  Projects,
   Resume,
   Contact,
   EarthCanvas,
